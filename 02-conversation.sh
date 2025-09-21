48b0967799fd61e64a3da947cc12f1c52a7f02e1
jaswanth kumar <jaswanthkumarjash2000@gmail.com>
2025-09-21 11:33:04 +0530

    02-conversation.sh file is committed first time

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "jaswanth:: Hi Kumar, How are you?"
+echo "kumar:: I'm good, thanks! How about you?"
+echo "jaswanth:: I'm doing well, thank you! What have you been up to lately?"
+echo "kumar:: I've been working on a new project at work. It's been quite"
