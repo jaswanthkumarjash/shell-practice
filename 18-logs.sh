7f630adc397a0411f42f9032c321f063527264a3
jaswanth kumar <jaswanthkumarjash2000@gmail.com>
2025-09-25 00:16:48 +0530

    Added new files

--- 18-logs.sh
+++ 18-logs.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 USERID=$(id -u)
-R="\E[31M"
-G="\E[32M"
-Y="\E[33M"
-N="\E[0m"
 
 LOGFOLDER="/var/log/shellscript"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
@@ -22,22 +18,27 @@ a=$1
 b=$2
 c=$3
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 INSTALL () {
     dnf list installed "$1" &>> $LOGFILE
     if [ $? -ne 0 ]; then
-        echo "Not Installed yet, We need to install"
+        echo "Not Installed yet, We need to install" | tee -a $LOGFILE
         dnf install "$1" -y &>> $LOGFILE
         if [ $? -ne 0 ]; then
-            echo -e "$2 ERROR:: $1 installation failed $5"
+            echo -e "$2 ERROR:: $1 installation failed $5" | tee -a $LOGFILE
             exit 1
             else
-                echo -e "$3 $1 installation completed $5"
+                echo -e "$3 $1 installation completed $5" | tee -a $LOGFILE
         fi
         else
-            echo -e "$4 Already installed $1, no need to install again $5"
+            echo -e "$4 Already installed $1, no need to install again $5" | tee -a $LOGFILE
         fi
 }
 
 INSTALL $a $R $G $Y $N
-INSTALL $b
-INSTALL $c
+INSTALL $b $R $G $Y $N
+INSTALL $c $R $G $Y $N
