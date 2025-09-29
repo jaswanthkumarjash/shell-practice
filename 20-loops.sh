dd609f3f57199217c8c6189a764ad12c2b3374d9
jaswanth kumar <jaswanthkumarjash2000@gmail.com>
2025-09-29 21:08:09 +0530

    Added new files

---
+++ 20-loops.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+LOGFOLDER="/var/log/shellscript"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOGFILE="$LOGFOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGFOLDER
+echo "Script started executing at: $(date)"
+
+if [ $USERID -ne 0 ]; then
+    echo  "Please run this script as root user or using sudo"
+    exit 1
+fi
+
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+INSTALL () {
+    dnf list installed "$1" &>> $LOGFILE
+    if [ $? -ne 0 ]; then
+        echo "Not Installed yet, We need to install" | tee -a $LOGFILE
+        dnf install "$1" -y &>> $LOGFILE
+        if [ $? -ne 0 ]; then
+            echo -e "$2 ERROR:: $1 installation failed $5" | tee -a $LOGFILE
+            exit 1
+            else
+                echo -e "$3 $1 installation completed $5" | tee -a $LOGFILE
+        fi
+        else
+            echo -e "$4 Already installed $1, no need to install again $5" | tee -a $LOGFILE
+        fi
+}
+
+for var in $@
+do
+INSTALL $var $R $G $Y $N
+done
