6ddaafefbd05ea9a3e20e604b1cfd6ba834515b1
jaswanth kumar <jaswanthkumarjash2000@gmail.com>
2025-11-23 20:55:06 +0530

    Shellscript practice

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+echo "Enter USERNAME to continue:"
+
+read USERNAME
+
 echo "Enter PIN NUMBER to continue:"
 
 read -s PIN_NUMBER
+
+echo "You entered: $USERNAME"
 
 echo "You entered: $PIN_NUMBER"
