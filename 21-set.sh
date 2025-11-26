69f8d0d6c8815102641f0b716cd735012b6782f9
jaswanth kumar <jaswanthkumarjash2000@gmail.com>
2025-11-26 22:35:20 +0530

    Shell script practice

---
+++ 21-set.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "The concept is about sets"
+echo "=========Before sets======="
+echo "Before ERROR"
+sldefrgthy
+echo "After ERROR"
+
+set -e
+echo "=========Before sets======="
+echo "Before ERROR"
+sldefrgthy
+echo "After ERROR"
